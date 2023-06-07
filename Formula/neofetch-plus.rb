2416261b5d25b231b60c646f2988c3a7c7899bc4
LanikSJ <laniksj@gmail.com>
2023-06-07 14:38:39 -0700

    neofetch-plus 7.2.4

--- Formula/neofetch-plus.rb
+++ Formula/neofetch-plus.rb
@@ -4,8 +4,8 @@
 class NeofetchPlus < Formula
   desc "Fast, highly customisable system info script based on neofetch"
   homepage "https://github.com/LanikSJ/neofetch"
-  url "https://github.com/LanikSJ/neofetch/archive/refs/tags/7.2.3.tar.gz"
-  sha256 "a11608242e732a71b2bd1c9774178783f021b73ed481a0d065c37f25846ef851"
+  url "https://github.com/LanikSJ/neofetch/archive/refs/tags/7.2.4.tar.gz"
+  sha256 "35b36cc441620d636bded68c89a1398de6fc04d3ac09df3dc67ab278cf9bfe39"
   license "MIT"
   head "https://github.com/LanikSJ/neofetch.git", branch: "master"
 
