c01930be19424cbee8c9c2405f1959f1e01e212b
LanikSJ <LanikSJ@users.noreply.github.com>
2023-10-19 03:51:26 -0700

    lsusb-plus 1.1 (#31)

--- Formula/lsusb-plus.rb
+++ Formula/lsusb-plus.rb
@@ -1,7 +1,7 @@
 class LsusbPlus < Formula
   desc "List USB devices, just like the Linux lsusb command"
   homepage "https://github.com/LanikSJ/lsusb"
-  url "https://github.com/LanikSJ/lsusb/archive/refs/tags/1.0.3.tar.gz"
+  url "https://github.com/LanikSJ/lsusb/archive/refs/tags/1.1.tar.gz"
   sha256 "af840036a710d7f166d7ee3c9b17e24f695c6a6f5eeb386671a58759fc57d2b0"
   license "MIT"
   head "https://github.com/LanikSJ/lsusb", using: :git
