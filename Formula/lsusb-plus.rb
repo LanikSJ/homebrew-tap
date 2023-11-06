52b227f58a508acaf423b3fd86d7dab5a12f8092
LanikSJ <laniksj@gmail.com>
2023-11-05 22:02:49 -0800

    Add Branch Main as Default Branch

--- Formula/lsusb-plus.rb
+++ Formula/lsusb-plus.rb
@@ -4,8 +4,8 @@ class LsusbPlus < Formula
   url "https://github.com/LanikSJ/lsusb/archive/refs/tags/1.1.1.tar.gz"
   sha256 "8d3a57cb8387ad53b146119c7863fea86a8312153e1256b6e6e83366b454bbc6"
   license "MIT"
-  head "https://github.com/LanikSJ/lsusb", using: :git
-
+  head "https://github.com/LanikSJ/lsusb.git", branch: "main"
+  
   depends_on :macos
 
   conflicts_with "lsusb", because: "both provide an `lsusb` binary"
