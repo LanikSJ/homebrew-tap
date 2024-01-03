ce8edaa16ae102df8bf2c335b89f73e938515fc6
LanikSJ <LanikSJ@users.noreply.github.com>
2024-01-02 17:19:07 -0800

    Update Name and Description of Android Messages (#47)
    Signed-off-by: LanikSJ <laniksj@gmail.com>

--- Casks/android-messages-plus.rb
+++ Casks/android-messages-plus.rb
@@ -6,8 +6,8 @@ cask "android-messages-plus" do
   sha256 "5f790e8265e1bafc2803e82252de6faa1356585283c766ac762a2d520970a3ea"
 
   url "https://github.com/LanikSJ/android-messages-desktop/releases/download/#{version}/Android-Messages-v#{version}-mac-universal.zip"
-  name "Android Messages"
-  desc "Android Messages as a Cross-platform Desktop App"
+  name "Android Messages Desktop"
+  desc "Desktop Client for Android Messages"
   homepage "https://github.com/LanikSJ/android-messages-desktop"
 
   conflicts_with cask: "android-messages,orangedrangon-android-messages"
