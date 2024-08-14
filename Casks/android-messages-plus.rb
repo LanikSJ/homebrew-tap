20142fc04c2ba34f9cbb617a721ba041aafbb10c
LanikSJ <LanikSJ@users.noreply.github.com>
2024-08-14 14:06:48 -0700

    android-messages-plus 5.4.5 (#70)

--- Casks/android-messages-plus.rb
+++ Casks/android-messages-plus.rb
@@ -1,6 +1,6 @@
 cask "android-messages-plus" do
-  version "5.4.4"
-  sha256 "ae3489e9db130613be9740239d98865996fe52ce7962e2b8655d54431facac47"
+  version "5.4.5"
+  sha256 "cea29854bcfec23936a3de4a22f2e9ed168fb5e9b1534661ca7e490294261753"
 
   url "https://github.com/LanikSJ/android-messages-desktop/releases/download/#{version}/Android-Messages-v#{version}-mac-universal.zip"
   name "Android Messages Desktop"
@@ -16,7 +16,6 @@ cask "android-messages-plus" do
     "android-messages",
     "orangedrangon-android-messages",
   ]
-
   depends_on macos: ">= :high_sierra"
 
   app "Android Messages.app"
