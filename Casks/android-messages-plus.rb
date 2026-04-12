36dfd341f089e44aadbbd81af663401cb5580541
LanikSJ <LanikSJ@users.noreply.github.com>
2026-04-12 15:35:57 -0700

    android-messages-plus 6.0.0 (#198)

--- Casks/android-messages-plus.rb
+++ Casks/android-messages-plus.rb
@@ -1,6 +1,6 @@
 cask "android-messages-plus" do
-  version "5.7.1.1"
-  sha256 "0dac3d263c17de61a6ed703690bdd7870b20cab59c190b9b187999699c27070e"
+  version "6.0.0"
+  sha256 "d81c3606bf468bd3843e30a2474b48e8c4d1941f8730abb80c46e8a3e5b776be"
 
   url "https://github.com/LanikSJ/android-messages-desktop/releases/download/#{version}/Android-Messages-v#{version}-mac-universal.zip"
   name "Android Messages Desktop"
