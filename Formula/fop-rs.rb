5adac42783595ab7326e1b3e22cc73912a264e25
LanikSJ <laniksj@gmail.com>
2026-04-30 19:35:52 -0700

    chore: bump fop-rs to v5.3.2 and update cask test workflow logic
    - update fop-rs to 5.3.2
    - update the cask install logic

--- Formula/fop-rs.rb
+++ Formula/fop-rs.rb
@@ -1,25 +1,25 @@
 class FopRs < Formula
   desc "Rust-based filter list optimizer for AdBlockers"
   homepage "https://github.com/ryanbr/fop-rs"
-  version "5.3.1"
+  version "5.3.2"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ryanbr/fop-rs/releases/download/v#{version}/fop-#{version}-macos-arm64"
-      sha256 "9cdce2b3463f4b81bd13558b3e166a8f1becac41556d8622a5746b4e7278cd45"
+      sha256 "62e43cccf3be6510b71bb3f179d07f2361293e5a10b7d7a606c659047240e999"
     else
       url "https://github.com/ryanbr/fop-rs/releases/download/v#{version}/fop-#{version}-macos-x86_64"
-      sha256 "2dc9fd1727b8472986f33625028c318a24f33f6b8582c7a934d4fc2f9fcac313"
+      sha256 "bd2a60deb7c142e3f54deba58747d5df3007a422f442dcde2d294e138ee6bd2e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ryanbr/fop-rs/releases/download/v#{version}/fop-#{version}-linux-arm64"
-      sha256 "bdefcf52b48cd98439d6d0995696d4673627ef58ce608be60121ec3b8a18d380"
+      sha256 "a1152ba15f1661ab9a5f03574180052f48fb293528c7387be214ce5b983c0373"
     else
       url "https://github.com/ryanbr/fop-rs/releases/download/v#{version}/fop-#{version}-linux-x86_64"
-      sha256 "ed3269663c14e91c388bb64138810f07f029ab01518c843f15d8c4c3f46940f3"
+      sha256 "631b5c93674be22fcdd38292a5a3c72786901781c41876747bc5ebbea5277cc0"
     end
   end
 
